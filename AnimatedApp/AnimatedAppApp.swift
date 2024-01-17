310e2cf3c83df47aa06599a2f2af676d49edd058
Nich <incuriousinic@gmail.com>
2024-01-17 23:29:28 +0100

    Initial commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by Nich on 2022-03-29.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
